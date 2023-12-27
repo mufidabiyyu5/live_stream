0880ae7ee4b1354f844a682ec4f0b95d6ed4011b
antoni <antoniyus.silaban@prisma-ads.com>
2023-12-27 13:57:16 +0700

    UPDATE

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const { createServer } = require('node:http');
+const { join } = require('node:path')
+const { Server } = require('socket.io')
+
+const app = express()
+const server = createServer(app)
+const io = new Server(server, {
+    connectionStateRecovery: {}
+})
+
+app.use(express.static(__dirname + '/public'));
+// app.use(express.static(path.join(__dirname, "js")));
+app.use("/asset", express.static("public"));
+
+app.get('/', (req, res) => {
+    res.sendFile(join(__dirname, 'index.html'))
+});
+
+app.get('/manual', (req, res) => {
+    res.sendFile(join(__dirname, 'manual.html'))
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected')
+    });
+    socket.on('chat message', (msg) => {
+        console.log('message', msg)
+        io.emit('chat message', msg)
+    })
+    socket.broadcast.emit('hi')
+})
+
+server.listen(3000, () => {
+    console.log('server running')
+})
